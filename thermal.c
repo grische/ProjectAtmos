9589901a7e1c9cd485f562fe707fb8c19f876d21
Stephan Rasp <raspstephan@gmail.com>
2012-10-30 14:23:21 +0100

    Strahlungstransport Eup - Fehler in Mu Integration

--- thermal.c
+++ thermal.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//#include <plplot/plplot.h>
 
 double planck (double T) {
   double B=(5.67E-8)*pow(T,4)/M_PI;
   return B;
 }
 
-/*
-void Lup (double *L, int nlyr, int ilyr, double dtau, const double *T, double Tsurf) {
-  L[ilyr]=planck(Tsurf)*exp(-dtau*(nlyr-ilyr))+
-    planck(T[ilyr])*(1.0-exp(-dtau*(nlyr-ilyr)));
-  
-  printf ("L[%d] = %f , T[%d] = %f, plank(T[%d]) = %f\n", ilyr, L[ilyr], ilyr, T[ilyr], ilyr, planck(T[ilyr]));  
-}
-*/
-
-double Lup (double tau, const double T, double Tsurf) {
-  double Lup = planck(Tsurf)*exp(-tau) + planck(T)*(1.0-exp(-tau));
-  printf ("Lup(): tau=%f, L = %f\n", tau, Lup);
+double Lup (double tau, const double Lbelow, const double Tlyr) {
+  double Lup = Lbelow * exp(-tau) + planck(Tlyr)*(1.0-exp(-tau));
+  //printf ("Lup(): L = %f, tau=%f, Lbelow=%f, Tlyr=%f\n", tau, Lup, Lbelow, Tlyr);
   return Lup;
 }
 
@@ -36,27 +26,46 @@ int main() {
 		,244.549807
 		,252.450005
   };
+
   const int nlyr = 10;
-  const double dtau =0.00000001/nlyr;
-
-  double *L=calloc(nlyr, sizeof(double));
-  double *Eup=calloc(nlyr, sizeof(double));
-  int ilyr;
-  //  int jlyr;
-  double mu;
+  const int nlev = nlyr+1;
   const double dmu = 0.01;
-  Eup [0]=0 ;
+  const double dtau =1.000/nlyr;
 
-  //  for (ilyr=0;ilyr<nlyr; ilyr++) {
-    for (mu=dmu; mu <= 1; mu += dmu) {
-      L[0]=Lup(dtau*(nlyr-0)/mu, T[0], Tsurf);
-      Eup[0] += L[0]*mu*dmu;
-    }
-    Eup[0] *= 2*M_PI;
-    //  }
-    printf ("Eup = %f\n" , Eup[0]);
+  double *L=calloc(nlev, sizeof(double));
+  double *Eup=calloc(nlyr, sizeof(double));
+
+  int ilyr;
+  double mu;
   
+  for(ilyr=0; ilyr < nlyr; ilyr++) {
+    Eup[ilyr] = 0;
+  }
 
+  //surface L
+  L[nlev-1] = planck(Tsurf);
 
+  /*
+   * UP
+   * ilev = 1, 2, ..., nlev-1 (=nlyr)
+   * ilyr = 1, 2, ..., nlyr-1
+   *
+   * Wegen up: ilev -1 = ilyr
+   *
+   * L[ilev] = L[ilev+1] exp(-dtau)  +  B(T[ilyr+1]) ( 1 - exp(-dtau) )
+   * quasi  L[ilyr] = L[ilyr+1] exp(-dtau)  +  B(T[ilyr]) (1 - exp(-dtau))
+   *
+   */
+  for (ilyr=nlyr-1;ilyr>0; ilyr--) {
+    // integration ueber raumwinkel -> 2*PI und Integration ueber mu
+    for (mu=dmu; mu <= 1; mu += dmu) {
+      L[ilyr]=Lup(dtau/mu, L[ilyr+1], T[ilyr]);
+      Eup[ilyr] += L[ilyr]*mu*dmu;
+    }
+    Eup[ilyr] *= 2*M_PI;
+    printf ("ilyr = %d, Eup = %f, Lbelow = %f\n" ,ilyr,  Eup[ilyr], L[ilyr]);
+  }
+
+  //printf("Die, die, die! %d %f\n", nlev+nlyr,L[nlev+nlyr+500]); 
   return 0;
 }
