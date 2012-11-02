8d2cad1a3a455bd88c9c00848675be7af432ab86
Stephan Rasp <raspstephan@gmail.com>
2012-11-02 13:37:02 +0100

    Edow hinzugefügt Enet berechnet Integration NICHT geändert... Fehler???

--- thermal.c
+++ thermal.c
@@ -7,11 +7,19 @@ double planck (double T) {
   return B;
 }
 
-double Lup (double tau, const double Lbelow, const double Tlyr) {
-  double Lup = Lbelow * exp(-tau) + planck(Tlyr)*(1.0-exp(-tau));
+double FLup (double tau, const double Lbelow, const double Tlyr) {
+  double FLup = Lbelow * exp(-tau) + planck(Tlyr)*(1.0-exp(-tau));
   //printf ("Lup(): L = %f, tau=%f, Lbelow=%f, Tlyr=%f\n", tau, Lup, Lbelow, Tlyr);
-  return Lup;
+  return FLup;
 }
+
+double FLdow (double tau, const double Labove, const double Tlyr) {
+  double FLdow = Labove * exp(-tau) + planck(Tlyr)*(1-exp(-tau));
+  return FLdow;
+
+
+}
+
 
 int main() {
   const double Tsurf = 255;
@@ -32,18 +40,24 @@ int main() {
   const double dmu = 0.01;
   const double dtau =1.000/nlyr;
 
-  double *L=calloc(nlev, sizeof(double));
+  double *Lup=calloc(nlev, sizeof(double));
+  double *Ldow=calloc(nlev, sizeof(double));
   double *Eup=calloc(nlyr, sizeof(double));
+  double *Edow=calloc(nlyr, sizeof(double));
+  double *Enet=calloc(nlyr, sizeof(double));
+
+
 
   int ilyr;
   double mu;
   
   for(ilyr=0; ilyr < nlyr; ilyr++) {
     Eup[ilyr] = 0;
+    Edow [ilyr] =0;
   }
 
   //surface L
-  L[nlev-1] = planck(Tsurf);
+  Lup[nlev-1] = planck(Tsurf);
 
   /*
    * UP
@@ -52,21 +66,50 @@ int main() {
    *
    * Wegen up: ilev -1 = ilyr
    *
-   * L[ilev] = L[ilev+1] exp(-dtau)  +  B(T[ilyr+1]) ( 1 - exp(-dtau) )
-   * quasi  L[ilyr] = L[ilyr+1] exp(-dtau)  +  B(T[ilyr]) (1 - exp(-dtau))
+   * Lup[ilev] = Lup[ilev+1] exp(-dtau)  +  B(T[ilyr+1]) ( 1 - exp(-dtau) )
+   * quasi  Lup[ilyr] = Lup[ilyr+1] exp(-dtau)  +  B(T[ilyr]) (1 - exp(-dtau))
    *
    */
   for (mu=dmu; mu <= 1; mu += dmu) {
-    for (ilyr=nlyr-1;ilyr>0; ilyr--) {
+    for (ilyr=nlyr-1;ilyr>=0; ilyr--) {
     // integration ueber raumwinkel -> 2*PI und Integration ueber mu
-      L[ilyr]=Lup(dtau/mu, L[ilyr+1], T[ilyr]);
-      Eup[ilyr] += L[ilyr]*mu*dmu*2.0*M_PI;
+      Lup[ilyr]=FLup(dtau/mu, Lup[ilyr+1], T[ilyr]);
+      Eup[ilyr] += Lup[ilyr]*mu*dmu*2.0*M_PI;
+    }
+  }
+  
+  //Calculation of Eup from surface
+
+   for (mu=dmu; mu <= 1; mu += dmu) {
+     Eup[nlyr]  = planck (Tsurf)*M_PI;
+       }
+  /*
+   * DOWN
+   * 
+   * Ldow[ilev] = Ldow[ilev-1] exp(-dtau) + B(T[ilyr-1]) ( 1-exp(-dtau))
+   * quasi Ldow[ilyr] = Ldow[ilyr-1] exp(-dtau) + B(T[ilyr]) (1-exp(-dtau))
+   *
+   */
+  // Ldow[0]=planck(T[0]);
+  //printf("T[0] = %f\n", T[0]);
+
+  for (mu=dmu; mu <= 1; mu += dmu) {
+    for (ilyr=0; ilyr<nlyr; ilyr++) {
+      
+      Ldow[ilyr]= FLdow(dtau/mu, Ldow[ilyr-1], T[ilyr]); 
+      Edow[ilyr] += Ldow[ilyr]*mu*dmu*2.0*M_PI;
     }
   }
 
-  // output of Eup and L
+  // Net Energy per layer: Enet
+
+  for (ilyr=0; ilyr<nlyr; ilyr++) {
+    Enet[ilyr]=Eup[ilyr+1]+Edow[ilyr-1]-Eup[ilyr]-Edow[ilyr];
+  }
+
+  // output of Eup, L, Edow and Enet
   for (ilyr=0; ilyr < nlyr; ilyr++) {
-    printf ("ilyr = %d, Eup = %f, Lbelow = %f\n" ,ilyr,  Eup[ilyr], L[ilyr]);
+    printf ("ilyr = %d, Eup = %f, Lbelow = %f,Edow = %f, Enet = %f\n" ,ilyr,  Eup[ilyr], Lup[ilyr], Edow[ilyr], Enet[ilyr]);
   }
 
   //printf("Die, die, die! %d %f\n", nlev+nlyr,L[nlev+nlyr+500]); 
