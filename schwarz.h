f3eebb75aad09ca5c38ba5798e2a7713480626c7
Christian Schmidbauer <ch.schmidbauer@gmail.com>
2012-11-04 16:52:53 +0100

    fix boltzmann, followup of a30538d6323b395fa0be708c332189013527ca8e

--- schwarz.h
+++ schwarz.h
@@ -1,8 +1,8 @@
 #ifndef SCHWARZ_H_INCLUDED
 #define SCHWARZ_H_INCLUDED
 
-double boltzman(const double T);
-double boltzman_overpi(const double T);
+double boltzmann(const double T);
+double boltzmann_overpi(const double T);
 int schwarzschild(const double tau, const double *T, const int nlev, const double Ts, double *edn, double *eup);
 
 #endif
